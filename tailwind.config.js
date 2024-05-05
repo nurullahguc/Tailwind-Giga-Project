42e71bf2f0f9d4f6f3cac26c14e3ee5d2fb2d2a4
NG <bbcn419a@gmail.com>
2024-05-05 21:24:30 +0300

    bg classlarının çalışmaması sorunu düzeltildi, tailwind.config.js içeriği yanlış yapılandırılmıştı. extend içne alınca düzeltildi

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,14 +15,17 @@ module.exports = {
             fontFamily: {
                 gemunu: ['Gemunu Libre', 'sans-serif'],
                 open: ['Open Sans', 'sans-serif']
+            },
+            colors: {
+                'gega-red': '#BC1A45',
+                'gega-melon': '#FFD369',
+                'gega-grey': '#DDDDDD',
+                'gega-white': '#F7F7F7',
+            },
+            spacing: {
+                128: '32rem'
             }
         },
-        colors: {
-            'gega-red': '#BC1A45',
-            'gega-melon': '#FFD369',
-            'gega-grey': '#DDDDDD',
-            'gega-white': '#F7F7F7',
-        }
     },
     plugins: [],
 }
